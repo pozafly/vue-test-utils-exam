877413827e46896a1426e60980277f1d54cfa94c
pozafly <pozafly@gmail.com>
2021-07-09 14:58:10 +0900

    Hook test

--- src/components/__test__/HelloJest2.test.js
+++ src/components/__test__/HelloJest2.test.js
@@ -6,7 +6,7 @@ describe('HelloJest', () => {
   beforeEach(() => {
     wrapper = shallowMount(HelloJest2);
   });
-  test('1', () => {
+  test('메세지 확인', () => {
     expect(wrapper.vm.msg).toBe('Hello Jest~');
   });
 });
